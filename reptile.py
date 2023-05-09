f4b81cd3d6379659ffdb2696320b42b048d988e6
jieran233 <jieranjiang233@gmail.com>
2023-05-09 13:23:27 +0800

    update

--- reptile.py
+++ reptile.py
@@ -1,40 +1,75 @@
+import sys
+
 import requests
 import json
 import re
-API = 'https://pigjpg.com/api/pic/?order=-create_time&page='
+import os
+import signal
 
-def getThisPagePic(page=1): # 此页图片爬取完毕返回True，此页不存在返回False，自动打印异常
-    url = 'https://pigjpg.com/api/pic/?order=-create_time&page=' + str(page)
+API = 'https://pigjpg.com/api/pic/?order=-create_time&page='
+OUTPUT_DIR = 'C:\\.cache\\imgs\\'
+
+stop_requested = False
+
+
+def sigint_handler(signum, frame):
+    global stop_requested
+    print(':: 已请求停止...')
+    stop_requested = True
+
+
+def getThisPagePic(_page=1):  # 此页图片爬取完毕返回True，此页不存在返回False，自动打印异常
+    global stop_requested
+    url = API + str(_page)
     # http的连接数超过最大限制，默认的情况下连接是Keep-alive的，所以这就导致了服务器保持了太多连接而不能再新建连接
     # 产生的连接数过多而导致Max retries exceeded，在header中不使用持久连接  'Connection': 'close' 或 requests.adapters.DEFAULT_RETRIES = 5
     headers = {'Connection': 'close'}
     requests.adapters.DEFAULT_RETRIES = 5
+    # 请求图片列表
     try:
         got = requests.get(url, headers=headers)
     except Exception as e:
-        print('\033[0;31m' + str(e.args) + '\033[0m')
+        print('\033[91m' + str(e.args) + '\033[00m')
     got_ = json.loads(got.text)
-    if(got_['pics'] == []):
-        return(False)
+    if not got_['pics']:
+        return False
     else:
         for i in range(len(got_['pics'])):
-            thisurl = got_['pics'][i]['info']['src'][-1][0]
-            thisfilename = re.findall('([^<>/\\\|:""\*\?]+\.\w+$)', thisurl)
-            thisfilename = thisfilename[0]
-            print(thisfilename)
-            try:
-                pic_data = requests.get(thisurl, headers=headers).content # 因为图片为流媒体文件，所以这里使用的是content，将它转为二进制
-            except Exception as e:
-                print('\033[0;31m' + str(e.args) + '\033[0m')
-            with open('imgs/{}'.format(thisfilename),'wb')as f: # 使用with open() 存储图片，wb标识符表示写二进制
-                f.write(pic_data)
-        return(True)
+            if stop_requested:
+                sys.exit(0)
 
-if(__name__ == '__main__'):
+            this_url = got_['pics'][i]['info']['src'][-1][0]
+            this_filename = re.findall(r'([^<>/\\\|:""\*\?]+\.\w+$)', this_url)
+            this_filename = this_filename[0]
+            this_filepath = os.path.join(OUTPUT_DIR, this_filename)
+            if os.path.exists(this_filepath):
+                print('IGNORE "{}" (already exist)'.format(this_filename))
+                continue
+            # 请求图片文件
+            try:
+                pic_data = requests.get(this_url, headers=headers).content  # 因为图片为流媒体文件，所以这里使用的是content，将它转为二进制
+            except Exception as e:
+                print('\033[91m' + str(e.args) + '\033[00m')
+            # 保存图片文件
+            with open(this_filepath, 'wb') as f:  # 使用with open() 存储图片，wb标识符表示写二进制
+                f.write(pic_data)
+            print('OK "{}"'.format(this_filename))
+        return True
+
+
+if __name__ == '__main__':
     page = 1
-    while(True):
-        print('\033[1;32;40m' + '开始爬取第 {} 页'.format(str(page)) + '\033[0m')
-        if(getThisPagePic(page)):
+
+    OUTPUT_DIR = os.path.realpath(OUTPUT_DIR)
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+
+    # 注册 SIGINT 信号处理函数
+    signal.signal(signal.SIGINT, sigint_handler)
+
+    while True:
+        print('\033[92m' + ':: 开始爬取第 {} 页'.format(str(page)) + '\033[00m')
+        if getThisPagePic(page):
             page = page + 1
         else:
             break
